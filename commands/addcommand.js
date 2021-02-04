d00b8b47388dddb4b3d54831800bea1c871c4294
jkyle109 <jkyle109@gmail.com>
2021-02-04 17:52:02 -0500

    fix for duplicate commands.

--- commands/addcommand.js
+++ commands/addcommand.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Adds a custom command.",
     args: true,
     hidden: false,
-    execute(message, args, client, db){
+    execute(message, args, client, d, db_commandsb) {
         // const commandName = args.shift();
         // const regex = new RegExp(`${commandName} +`);
         // const commandMessage = message.content.slice(process.env.PREFIX.length).split(regex, 2)[1];
@@ -12,22 +12,38 @@ module.exports = {
         require("dotenv").config();
         const prefix = process.env.PREFIX;
 
-        if(!(args.length>=2)){
+        if (!(args.length >= 2)) {
             message.reply("Not enough arguments provided.");
             return;
         }
-        
+
         const commandName = args.shift();
-        let regex = new RegExp(commandName + " (.+)" )      // RegExp to split by first instance of a phrase
-        const commandMessage = message.content.slice(process.env.PREFIX.length).split(regex)[1];
-        console.log("???? " + commandName + " " + commandMessage)
-        const query = `INSERT INTO custom_commands (command_name, command_message) VALUES ($$${commandName}$$, $$${commandMessage}$$)`;
-        console.log(query)
-        db.query(query, (err, res) => {
-            if(err){
-                throw err;
+        let regex = new RegExp(commandName + " (.+)"); // RegExp to split by first instance of a phrase
+        const commandMessage = message.content
+            .slice(process.env.PREFIX.length)
+            .split(regex)[1];
+        console.log("???? " + commandName + " " + commandMessage);
+        try {
+            if (commandName in db_commands) {
+                // Checks for database commands
+                message.reply(
+                    `There is already a custom command called "${commandName}".`
+                );
+                return;
+            } else {
+                const query = `INSERT INTO custom_commands (command_name, command_message) VALUES ($$${commandName}$$, $$${commandMessage}$$)`;
+                console.log(query);
+                db.query(query, (err, res) => {
+                    if (err) {
+                        throw err;
+                    }
+                    message.reply(
+                        `The command "${prefix}${commandName}" has been added.`
+                    );
+                });
             }
-            message.reply(`The command "${prefix}${commandName}" has been added.`);
-        })
-    }
-}
+        } catch (err) {
+            message.reply(`Something broke :o`);
+        }
+    },
+};
